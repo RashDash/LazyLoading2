e92c5ec7c9672d6ce3839fca133fe03d6bb78920
Ashwani Choudhary <ashwanichoudhary25@gmail.com>
2015-09-06 11:03:27 +0530

    Initial Commit

---
+++ LazyLoading2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LazyLoading2
+//
+//  Created by ok on 9/6/15.
+//  Copyright (c) 2015 Choudhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LazyLoading2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LazyLoading2
+//
+//  Created by ok on 9/6/15.
+//  Copyright (c) 2015 Choudhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
